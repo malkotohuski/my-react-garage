572e9d71662cd51e1c2b25f3ce675650c122a7f0
Daniel Dimitrov <malkotohuski@gmail.com>
2023-10-26 15:21:00 +0300

    new file with menu items

--- src/Componets/Pages/Home/Home.js
+++ src/Componets/Pages/Home/Home.js
@@ -1,54 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import menuItems from './MenuItems';
 import './Home.css';
 
 const images = [
   'https://wieck-nissanao-production.s3.amazonaws.com/photos/50ec55516d7fa6d3b43dcf4dd2b8e6183a4abc04/preview-928x522.jpg', // Replace with the URLs or paths to your images
   'https://hips.hearstapps.com/hmg-prod/images/2021-porsche-911-turbo-s-pdk-130-edit-1608061336.jpg?crop=0.622xw:0.466xh;0.376xw,0.500xh&resize=1200:*',
   'https://media.istockphoto.com/id/488467276/photo/man-calling-for-help.jpg?s=2048x2048&w=is&k=20&c=vzvZm9_SMI0CHJTpWfu_j4cgbgFytbVtphMrZxh3rCQ='
-];
-
-const menuItems = [
-  {
-    image: 'https://img.freepik.com/free-vector/realistic-complete-set-car-wheels_1284-29765.jpg?size=626&ext=jpg',
-    label: 'Tires',
-  },
-  {
-    image: 'https://s.aolcdn.com/commerce/blogcdn/www.autoblog.com/media/2006/09/advancedvtec3.jpg',
-    label: 'Engine',
-  },
-  {
-    image: 'https://upload.wikimedia.org/wikipedia/commons/2/21/Mercedes-Benz_7G-Tronic_transmission.jpg',
-    label: 'Gearbox',
-  },
-  {
-    image: 'https://di-uploads-pod16.dealerinspire.com/toyotaofnorthcharlotte/uploads/2020/03/N-Charlotte-car-suspension-system.jpg',
-    label: 'Suspension',
-  },
-  {
-    image: 'https://autotrends.org/wp-content/uploads/motor-oil-weights.jpg',
-    label: 'Motor Oil',
-  },
-  {
-    image: 'https://c8.alamy.com/comp/2BB3X6C/braking-system-car-brake-disks-with-different-perforations-and-calipers-isolated-on-white-background-3d-illustration-2BB3X6C.jpg',
-    label: 'Braking System',
-  },
-  {
-    image: 'https://carauto.bg/ckfinder/userfiles/images/3.jpg',
-    label: 'Ignition And Heating Systems',
-  },
-  {
-    image: 'https://www.avtotachki.com/wp-content/uploads/2021/01/konstrukcija-kuzova-avtomobilya.jpg',
-    label: 'Car body',
-  },
-  {
-    image: 'https://maslata.bg/wp-content/uploads/2015/03/1.jpg',
-    label: 'Filter',
-  },
-  {
-    image: 'https://i.gaw.to/content/photos/26/98/269855_La_petite_histoire_de_la_direction_assistee.jpg?1024x640',
-    label: 'Steering Suspension',
-  },
 ];
 
 function HomePage() {
